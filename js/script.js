f97ea7b65f1414b7f0e9cfd272097d783f6f273a
DylanBlanco <dylanblanco1198@gmail.com>
2025-02-28 17:08:18 +0100

    const - let - var

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 document.getElementById('text').innerHTML='Hello World!';
+
+let nomeUno = 'Dylan';
+console.log(nomeUno);
+
+let text = 'si esercita';
+console.log(text);
+
+let newVar = nomeUno + ' ' + text;
+console.log(newVar);
